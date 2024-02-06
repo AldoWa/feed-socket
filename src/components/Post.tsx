748f5264c3e86b8cc854744e076a25c3c046e4ce
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-02-06 18:30:24 -0300

    :sparkles: feat: adding skeleton

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -23,15 +23,24 @@ export function Post() {
     date: new Date(),
   });
 
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
     setUser(generateFakeUser())
     setPost(generateFakePost())
+    setLoading(false)
   }, [])
+
+
+
+  if (loading) {
+    return <PostSkeleton />
+  }
 
   return (
     <section className='bg-[#323238] rounded-lg p-10 max-w-[832px]' >
       <div className='flex items-center justify-between'>
-        <div className='flex items-center '>
+        <div className='flex items-center'>
           <div className='mr-4 border-2 border-[#00B37E] p-[6px] rounded-lg'>
             <Image
               alt='User avatar'
@@ -48,7 +57,7 @@ export function Post() {
         </div>
         <p>{formatterDateDistance(post.date)}</p>
       </div>
-      
+
       <p className='max-w-[752px] text-[#C4C4CC] text-base mt-7'>
         {post.content}
       </p>
@@ -56,3 +65,23 @@ export function Post() {
   );
 }
 
+function PostSkeleton() {
+  const shimmer =
+    'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent';
+  return (<section className={`${shimmer} relative overflow-hidden rounded-lg p-10 min-w-[832px] bg-[#323238] shadow-sm`}>
+    <div className='flex items-center'>
+      <div className='mr-4 border-2 border-[#00B37E] p-[6px] rounded-lg'>
+        <div className='w-10 h-10 bg-gray-300 rounded-md'></div>
+      </div>
+      <div>
+        <div className='w-20 h-4 bg-gray-300 rounded-md'></div>
+        <div className='w-10 h-3 bg-gray-300 rounded-md mt-2'></div>
+      </div>
+    </div>
+    <div className='w-full h-4 bg-gray-300 rounded-md mt-7'></div>
+    <div className='w-full h-4 bg-gray-300 rounded-md mt-4'></div>
+    <div className='w-full h-4 bg-gray-300 rounded-md mt-4'></div>
+    <div className='w-full h-4 bg-gray-300 rounded-md mt-4'></div>
+    <div className='w-full h-4 bg-gray-300 rounded-md mt-4'></div>
+  </section>)
+}
